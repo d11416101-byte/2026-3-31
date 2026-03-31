033f4b1661a8e299ac3186e568d6a5ca918a6329
d11416101-byte <d11416101@gs.takming.edu.tw>
2026-03-31 14:06:15 +0800

    Add files via upload

---
+++ 2026-3-31/2026-3-31/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Student
+{
+    private int ID;
+    private string Name;
+    private Test Score; // 假設有一個 Test 類別處理成績
+
+    public static int count; // 靜態變數，用於記錄實例化的次數
+
+    public Student(int id, string n, int m, int f)
+    {
+        ID = id;
+        Name = n;
+        Score = new Test(m, f); // 建立 Test 物件實例
+        count++; // 每建立一個學生，計數器加 1
+    }
+
+    public string GetStudent()
+    {
+        string str;
+        str = "學生編號: " + ID.ToString();
+        str += "\n學生姓名: " + Name;
+        str += "\n期中考成績: " + Score.GetMid().ToString();
+        str += "\n期末考成績: " + Score.GetFinal().ToString();
+        str += "\n平均成績: " + Score.GetAvg().ToString();
+        return str;
+    }
+}
---
+++ 2026-3-31/2026-3-31/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2026_3_31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student joe, mary;
+
+            joe = new Student(1234, "AAA", 92, 88);
+            Console.WriteLine(joe.GetStudent());
+
+            mary = new Student(1234, "AAA", 92, 88);
+
+            Console.WriteLine(Student.count.ToString());
+        }
+    }
+}
